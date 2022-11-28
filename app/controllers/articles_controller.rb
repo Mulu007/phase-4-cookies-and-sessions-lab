3fad13306d900a32a3d67d50609e041020e9bc5d
Mulu <brianmulu007@gmail.com>
2022-11-28 17:02:54 +0300

    Ngori

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,14 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] >= 3
+      render json: {error: "unauthorized"}, status: :unauthorized
+    else
+      render json: article
+    end
+    byebug
   end
 
   private
